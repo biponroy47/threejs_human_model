1a0ced9ed9561fd6f4051193333b3cc084b58c41
Bipon Roy <biponroy47@yahoo.com>
2024-08-15 18:38:48 -0400

    fixed toggle handlers, added skeleton drawing

--- tests/tensorflow/src/components/VideoUploadPlayer/VideoUploadPlayer.jsx
+++ tests/tensorflow/src/components/VideoUploadPlayer/VideoUploadPlayer.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import './VideoUploadPlayer.css'
 import * as tf from '@tensorflow/tfjs'
 import * as poseDetection from '@tensorflow-models/pose-detection'
-import '@tensorflow/tfjs-backend-webgl' // Import the WebGL backend
+import '@tensorflow/tfjs-backend-webgl'
 
 const VideoUploadPlayer = ({ videoIndex }) => {
   const [videoSrc, setVideoSrc] = useState(null)
@@ -10,6 +10,7 @@ const VideoUploadPlayer = ({ videoIndex }) => {
   const canvasRef = useRef(null)
   const [detector, setDetector] = useState(null)
   const [isMoveNetActive, setIsMoveNetActive] = useState(false)
+  const animationIdRef = useRef(null)
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0]
@@ -17,15 +18,14 @@ const VideoUploadPlayer = ({ videoIndex }) => {
       const videoURL = URL.createObjectURL(file)
       setVideoSrc(videoURL)
     } else {
-      alert('Please upload a valid video file.')
+      alert('Invalid video file.')
     }
   }
 
   useEffect(() => {
     const loadModel = async () => {
-      await tf.ready() // Ensure TensorFlow.js is fully initialized
-      await tf.setBackend('webgl') // Explicitly set the backend to 'webgl' or another supported backend
-
+      await tf.ready()
+      await tf.setBackend('webgl')
       const detectorConfig = {
         modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
       }
@@ -35,98 +35,91 @@ const VideoUploadPlayer = ({ videoIndex }) => {
       )
       setDetector(detector)
     }
-
     loadModel()
   }, [])
 
-  const detectPose = async () => {
-    console.log('DetectPose function started')
-    // console.log(detector)
-    // console.log(videoRef.current)
-    // console.log(isMoveNetActive)
-    // console.log('Video ready state:', videoRef.current.readyState)
-    if (detector && videoRef.current && !isMoveNetActive) {
-      const video = videoRef.current
-      console.log('Video ready state:', video.readyState)
+  useEffect(() => {
+    const detectPose = async () => {
+      if (detector && videoRef.current && isMoveNetActive) {
+        const video = videoRef.current
+        const canvas = canvasRef.current
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
+        const ctx = canvas.getContext('2d')
 
-      const canvas = canvasRef.current
-      const ctx = canvas.getContext('2d')
-
-      canvas.width = video.videoWidth
-      canvas.height = video.videoHeight
-
-      const detect = async () => {
-        console.log('Detect function running')
-        //console.log('Video ready state:', video.readyState)
-        if (video.readyState >= 2) {
-          // console.log('Video is ready, estimating poses')
+        const detect = async () => {
           const poses = await detector.estimatePoses(video)
-          // console.log('Detected poses:', poses) // Should output detected poses
-          ctx.clearRect(0, 0, canvas.width, canvas.height) // Clear the previous frame
-          drawPoses(poses, ctx) // Draw new poses
-
-          requestAnimationFrame(detect) // Continue detecting for the next frame
+          ctx.clearRect(0, 0, canvas.width, canvas.height)
+          drawPoses(poses, ctx)
+          animationIdRef.current = requestAnimationFrame(detect)
         }
+        detect()
       }
-      console.log(
-        'Canvas width:',
-        canvas.width,
-        'Canvas height:',
-        canvas.height
-      )
-      console.log(
-        'Video width:',
-        video.videoWidth,
-        'Video height:',
-        video.videoHeight
-      )
-
-      detect() // Start detection
     }
-  }
+    isMoveNetActive ? detectPose() : stopDetection()
+  }, [isMoveNetActive, detector])
 
   const drawPoses = (poses, ctx) => {
-    const baseSize = 5 // Base dot size
-    const scaleX = canvasRef.current.width / 640 // Assuming 640 is the standard width
-    const scaleY = canvasRef.current.height / 480 // Assuming 480 is the standard height
-    const dotSize = baseSize * Math.min(scaleX, scaleY) // Scale dot size proportionally
-    // console.log('Detected poses:', poses) // Log the poses array
-    poses.forEach((pose) => {
-      pose.keypoints.forEach((keypoint) => {
-        if (keypoint.score > 0.01) {
-          // console.log(`Drawing keypoint at (${keypoint.x}, ${keypoint.y})`) // Log each keypoint position
-          // Only draw keypoints with a high confidence score
-          const { x, y } = keypoint
+    const baseSize = 5
+    const baseLineWidth = 3
+    const scaleX = canvasRef.current.width / 640
+    const scaleY = canvasRef.current.height / 480
+    const dotSize = baseSize * Math.min(scaleX, scaleY)
+    const lineWidth = baseLineWidth * Math.min(scaleX, scaleY)
+
+    const drawKeypoint = (keypoint, ctx) => {
+      const { x, y } = keypoint
+      ctx.beginPath()
+      ctx.arc(x, y, dotSize, 0, 2 * Math.PI)
+      ctx.fillStyle = 'red'
+      ctx.fill()
+    }
+
+    const drawSkeleton = (keypoints, ctx) => {
+      const adjacentKeyPoints = poseDetection.util.getAdjacentPairs(
+        poseDetection.SupportedModels.MoveNet
+      )
+      ctx.lineWidth = lineWidth
+      ctx.strokeStyle = 'red'
+
+      adjacentKeyPoints.forEach(([i, j]) => {
+        const kp1 = keypoints[i]
+        const kp2 = keypoints[j]
+
+        if (kp1.score > 0.5 && kp2.score > 0.5) {
           ctx.beginPath()
-          ctx.arc(x, y, dotSize, 0, 2 * Math.PI)
-          ctx.fillStyle = 'red'
-          ctx.fill()
+          ctx.moveTo(kp1.x, kp1.y)
+          ctx.lineTo(kp2.x, kp2.y)
+          ctx.stroke()
         }
       })
+    }
+
+    poses.forEach((pose) => {
+      pose.keypoints.forEach((keypoint) => {
+        if (keypoint.score > 0.5) {
+          drawKeypoint(keypoint, ctx)
+        }
+      })
+      drawSkeleton(pose.keypoints, ctx)
     })
   }
 
-  const handleMoveNetToggle = () => {
-    setIsMoveNetActive(!isMoveNetActive)
-    if (!isMoveNetActive) {
-      console.log(`Video ${videoIndex}: MoveNet is now ON`)
-      videoRef.current.play()
-      detectPose() // Start pose detection when MoveNet is enabled
-    } else {
-      console.log(`Video ${videoIndex}: MoveNet is now OFF`)
+  const stopDetection = () => {
+    if (animationIdRef.current) {
+      cancelAnimationFrame(animationIdRef.current)
+      const ctx = canvasRef.current.getContext('2d')
+      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
     }
   }
 
-  const buttonHandlers = [
-    () => {
-      setIsMoveNetActive(false)
-      console.log(`Video ${videoIndex}: MoveNet is turned OFF`)
-    }, // Button 1: OFF
-    handleMoveNetToggle, // Button 2: MoveNet
-    () => alert(`Video ${videoIndex}: PoseNet action!`),
-    () => alert(`Video ${videoIndex}: BlazePose action!`),
-    () => alert(`Video ${videoIndex}: BodyPix action!`),
-  ]
+  const handleOffClick = () => {
+    setIsMoveNetActive(false)
+  }
+
+  const handleMoveNetToggle = () => {
+    setIsMoveNetActive((prevIsMoveNetActive) => !prevIsMoveNetActive)
+  }
 
   return (
     <div className={`video-upload-container ${videoSrc ? 'uploaded' : ''}`}>
@@ -136,7 +129,6 @@ const VideoUploadPlayer = ({ videoIndex }) => {
         accept='video/*'
         onChange={handleFileUpload}
       />
-
       {videoSrc && (
         <>
           <div className='video-container'>
@@ -151,26 +143,25 @@ const VideoUploadPlayer = ({ videoIndex }) => {
           </div>
           <div className='button-row'>
             <div className='button-with-label'>
-              <button onClick={buttonHandlers[0]}>OFF</button>
+              <button onClick={handleOffClick}>OFF</button>
             </div>
             <div className='button-with-label'>
               <button
                 className={isMoveNetActive ? 'active' : ''}
-                onClick={buttonHandlers[1]}>
+                onClick={handleMoveNetToggle}>
                 MoveNet
               </button>
             </div>
             <div className='button-with-label'>
-              <button onClick={buttonHandlers[2]}>PoseNet</button>
+              <button onClick={() => console.log('temp')}>PoseNet</button>
             </div>
             <div className='button-with-label'>
-              <button onClick={buttonHandlers[3]}>BlazePose</button>
+              <button onClick={() => console.log('temp')}>BlazePose</button>
             </div>
             <div className='button-with-label'>
-              <button onClick={buttonHandlers[4]}>BodyPix</button>
+              <button onClick={() => console.log('temp')}>BodyPix</button>
             </div>
           </div>
-
           <canvas
             ref={canvasRef}
             className='pose-overlay'
